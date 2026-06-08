ba74d2dd34225fec0095ba5aef90712518e44d06
msuski <01187288@pw.edu.pl>
2026-06-08 17:13:29 +0200

    Added top bar with back, edit and delete buttons

--- mobile_app/app/src/main/java/routor/src/screens/routeDetails/RouteDetailsScreen.kt
+++ mobile_app/app/src/main/java/routor/src/screens/routeDetails/RouteDetailsScreen.kt
@@ -9,7 +9,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -23,15 +32,18 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import routor.src.dialogFactory.confirmDialog.ConfirmDialog
 import routor.src.utils.MapHelper
 import routor.src.utils.formatTime
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RouteDetailsScreen(
     displayRoutesScreen: () -> Unit,
     viewModel: RouteDetailsViewModel
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val routeDialogState by viewModel.routeDialogState.collectAsStateWithLifecycle()
 
     LaunchedEffect(viewModel.routeDeletedEvent) {
         viewModel.routeDeletedEvent.collect {
@@ -46,74 +58,114 @@ fun RouteDetailsScreen(
     }
     MapHelper.SetupMapLifecycleEvents(mapView)
 
-    // TODO
-
-    when (val currentState = uiState) {
-        RouteDetailsUiState.Loading -> {
-        }
-        RouteDetailsUiState.Error -> {
-            Text("Route not found")
-        }
-        is RouteDetailsUiState.Success -> {
-            MapHelper.displayFullRoute(mapView, currentState.points)
-
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-            ) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .weight(3f),
-                ) {
-                    AndroidView(
-                        factory = { mapView },
-                        update = {}
-                    )
-                }
-                Spacer(
-                    modifier = Modifier
-                        .padding(5.dp)
-                )
-                Text(
-                    modifier = Modifier
-                        .align(Alignment.CenterHorizontally),
-                    style = TextStyle(fontSize = 25.sp, fontWeight = FontWeight.Bold),
-                    text = currentState.route.name
-                )
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .weight(2f)
-                        .padding(20.dp)
-                ) {
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.SpaceBetween
-                    ) {
-                        //stats
-                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                            Text(
-                                text = "STATS",
-                                style = TextStyle(fontWeight = FontWeight.Light, fontSize = 12.sp)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    if (uiState is RouteDetailsUiState.Success) {
+                        Text(
+                            text = (uiState as RouteDetailsUiState.Success).route.name,
+                            style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                        )
+                    } else {
+                        Text(text = "Route Details")
+                    }
+                },
+                navigationIcon = {
+                    IconButton(onClick = displayRoutesScreen) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back"
+                        )
+                    }
+                },
+                actions = {
+                    if (uiState is RouteDetailsUiState.Success) {
+                        IconButton(onClick = { viewModel.onEvent(RouteDetailsEvent.ShowEditRouteDialog) }) {
+                            Icon(
+                                imageVector = Icons.Default.Edit,
+                                contentDescription = "Edit"
                             )
+                        }
+                        IconButton(onClick = { viewModel.onEvent(RouteDetailsEvent.ShowDeleteRouteDialog) }) {
+                            Icon(
+                                imageVector = Icons.Default.Delete,
+                                contentDescription = "Delete"
+                            )
+                        }
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
 
-                            Spacer(modifier = Modifier.height(16.dp))
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            when (val currentState = uiState) {
+                RouteDetailsUiState.Loading -> {
+                }
+                RouteDetailsUiState.Error -> {
+                    Text("Route not found")
+                }
+                is RouteDetailsUiState.Success -> {
+                    MapHelper.displayFullRoute(mapView, currentState.points)
 
-                            Row(
-                                modifier = Modifier.fillMaxWidth(),
-                                horizontalArrangement = Arrangement.SpaceEvenly
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .weight(3f),
+                        ) {
+                            AndroidView(
+                                factory = { mapView },
+                                update = {}
+                            )
+                        }
+                        Spacer(
+                            modifier = Modifier
+                                .padding(5.dp)
+                        )
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .weight(2f)
+                                .padding(20.dp)
+                        ) {
+                            Column(
+                                modifier = Modifier.fillMaxSize(),
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.SpaceBetween
                             ) {
-                                StatItem(
-                                    label = "DISTANCE",
-                                    value = "${"%.2f".format(currentState.route.distanceKm)} km"
-                                )
-                                StatItem(
-                                    label = "AVERAGE SPEED",
-                                    value = "${"%.2f".format(currentState.route.distanceKm / (currentState.route.duration * 60 * 60))} km/h"
-                                )
-                                StatItem(label = "DURATION", value = formatTime(currentState.route.duration))
+                                //stats
+                                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                                    Text(
+                                        text = "STATS",
+                                        style = TextStyle(fontWeight = FontWeight.Light, fontSize = 12.sp)
+                                    )
+
+                                    Spacer(modifier = Modifier.height(16.dp))
+
+                                    Row(
+                                        modifier = Modifier.fillMaxWidth(),
+                                        horizontalArrangement = Arrangement.SpaceEvenly
+                                    ) {
+                                        StatItem(
+                                            label = "DISTANCE",
+                                            value = "${"%.2f".format(currentState.route.distanceKm)} km"
+                                        )
+                                        StatItem(
+                                            label = "AVERAGE SPEED",
+                                            value = "${"%.2f".format(currentState.route.distanceKm / (currentState.route.duration * 60 * 60))} km/h"
+                                        )
+                                        StatItem(label = "DURATION", value = formatTime(currentState.route.duration))
+                                    }
+                                }
                             }
                         }
                     }
@@ -122,15 +174,10 @@ fun RouteDetailsScreen(
         }
     }
 
-
-
-//    if(routeDialogState.isVisible){
-//        ConfirmDialog(routeDialogState.config!!)
-//    }
+    if (routeDialogState.isVisible && routeDialogState.config != null) {
+        ConfirmDialog(routeDialogState.config!!)
+    }
 }
-
-
-
 
 @Composable
 private fun StatItem(label: String, value: String) {
